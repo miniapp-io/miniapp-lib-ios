34e44421f7d85e906fe046558e6d475edf0ab771
biu <biu@mtsocialdao.com>
2025-08-04 21:25:05 +0800

    1.0.8

--- OpenPlatform/MiniApp/UI/WebAppController.swift
+++ OpenPlatform/MiniApp/UI/WebAppController.swift
@@ -2168,7 +2168,7 @@ extension WebAppController.Node : WKNavigationDelegate, WKUIDelegate{
             decisionHandler(.cancel)
             MiniAppServiceImpl.instance.openUrl(viewController: self.controller, url: url, webLaunchParams: self.controller?.webAppParameters)
         } else {
-            if (!iisNormalScheme(url) && !isLocalFile(url) && !isBlank(url)) {
+            if (!isNormalScheme(url) && !isLocalFile(url) && !isBlank(url)) {
                 MiniAppServiceImpl.instance.openUrl(viewController: self.controller, url: url, webLaunchParams: self.controller?.webAppParameters)
                 decisionHandler(.cancel)
             } else {
